e897233dac9ad3c8bc5040fb16f7660251397cce
posridha <poornimasridhar@in.ibm.com>
2018-12-11 00:13:09 +0530

    Update listevents.js

--- listevents.js
+++ listevents.js
@@ -1,32 +1,34 @@
+// statisticsCtrl.js
+const path = require("path");
+const log = require("npmlog");
+const async = require("async");
+const request = require("request");
+const cloudant = require("cloudant");
+const dateFormat = require("date-and-time");
+const config = require("../../config.js");
+const auth = require("../../auth.js");
+var dateTime = require('node-datetime');
 
+ function getListofStaffs(req,clearanceLevel, callback) {}
+
+function getListofEvents(req) {}
 
 exports.listevents = function (req, res) {
     log.info(path.basename(module.filename), "Staff List :: getlistevent())");
-
-
     async.waterfall([
-        getlistofStaffs.bind(null, req),
-        getlistofEvents.bind(null, req),
-        //getPremise.bind(null, req),
-        //getCountry.bind(null, req),
-        //getEntity.bind(null, req)
-
-    ], function (err, result) {
-        if (err) {
-
-            res.status(500).send({ statusMessage: "Error occured while processing the List of Events API request." });
-        } else {
-            console.log(result);
-            console.log("Success staff records");
-            res.status(200).send(result);
-        }
-    });
+        getListofStaffs.bind(null, req),
+        getListofEvents.bind(null, req)
+      ], (err, results) => {
+        if (err) return next(err);
+        return res.send(results);
+      });
 };
 
 
-function getlistofStaffs(req, callback) {
-    log.info(path.basename(module.filename), "List Staffs :: getlistofStaffs()");
-    var listofStaffsAPI = www.gmail.com;
+function getlistofStaffs(req, clearanceLevel,callback) {
+    log.info(path.basename(module.clearanceLevel), "List Staffs :: getlistofStaffs()");
+    var listofStaffsAPI = config.composerRestServer.url + "/queries/getStaffClearanceByStaffId?staffId=resource%3Aorg.de.eclearance.Staff%23" + req.user.staffID;
+    //var listofEventsAPI = config.composerRestServer.url + "/queries/getStaffClearanceByStaffId?staffId=resource:org.de.eclearance.Staff#Hi " + "0001"; 
 
 
     const requestOptions = {
@@ -52,7 +54,8 @@ function getlistofEvents(req, callback) {
     var formatted = dt.format('Y-m-d');
     console.log(formatted);
     log.info(path.basename(module.filename), "List Events :: getlistofEvents()");
-    var listofEventsAPI = www.gmail.com;
+    var listofEventsAPI = config.composerRestServer.url + "/queries/getAllFutureEvents?startDate=" + formatted;
+
 
 
     const requestOptions = {
@@ -70,4 +73,96 @@ function getlistofEvents(req, callback) {
         }
     });
 }
+
+function getlistofEvents(req, callback) {
+    var dt = dateTime.create();
+    var formatted = dt.format('Y-m-d ');
+    console.log(formatted);
+    log.info(path.basename(module.filename), "List Events :: getlistofEvents()");
+    var listofEventsAPI = config.composerRestServer.url + "/queries/getAllFutureEvents?startDate=" + formatted;
+
+
+
+    const requestOptions = {
+        url: listofEventsAPI,
+        json: true
+    };
+
+
+    request(requestOptions, function (err, res, result) {
+        if (err) {
+            return callback(true, null);
+        } else if (res.statusCode == 200) {
+
+            return callback(null, result);
+        }
+    });
+}
+
+function getPremise(req, callback) {
+    log.info(path.basename(module.filename), "List Premise :: getPremise()");
+    var listPremiseAPI = config.composerRestServer.url + "/Premise" + "/P001";
+    const requestOptions = {
+        url: listPremiseAPI,
+        json: true
+    };
+
+    request(requestOptions, function (err, res, data) {
+        if (err) {
+            return callback(true, null);
+        } else if (res.statusCode == 200) {
+            var premiseentity = data.entity;
+            //var premiseentit1 = premiseentity.slice(20,0);
+
+            var premisecountry = data.country;
+            var premiseaddress = data.address;
+
+            return callback(null, { premiseentity: data.entity, premisecountry: data.country, premiseaddress: data.address });
+            //return callback(null,data);
+        }
+    });
+}
+
+function getCountry(req, callback) {
+    log.info(path.basename(module.filename), "List Country :: getCountry()");
+    var listCountryAPI = config.composerRestServer.url + "/Country" + "/C001";
+    const requestOptions = {
+        url: listCountryAPI,
+        json: true
+    };
+
+    request(requestOptions, function (err, res, result) {
+        if (err) {
+            return callback(true, null);
+        } else if (res.statusCode == 200) {
+            var countrycountryId = result.countryId;
+            var countrycountryName = result.countryName;
+
+            return callback(null, { countrycountryId: result.countryId, countrycountryName: result.countryName });
+        }
+    });
+}
+
+function getEntity(req, callback) {
+    log.info(path.basename(module.filename), "List Entity :: getEntity()");
+    var listEntityAPI = config.composerRestServer.url + "/Entity" + "/E001";
+    const requestOptions = {
+        url: listEntityAPI,
+        json: true
+    };
+
+    request(requestOptions, function (err, res, result) {
+        if (err) {
+            return callback(true, null);
+        } else if (res.statusCode == 200) {
+            var entityentityId = result.entityId;
+            var entityname = result.name;
+
+            return callback(null, { entityentityId: result.entityId, entityname: result.name });
+        }
+    });
+}
+
+
+
 
